abf01aa5aaab5a4e2030123e93db2e2c0d1a4e15
maxime fontana <maximefontana@hotmail.com>
2019-12-19 14:04:26 -0800

    add quotaguard

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,12 +1,12 @@
 Geocoder.configure(
   # Geocoding options
-  # timeout: 10,                 # geocoding service timeout (secs)
+  # timeout: 30,                 # geocoding service timeout (secs)
   # lookup: :bing,         # name of geocoding service (symbol)
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
-  # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
+  # https_proxy: ENV['QUOTAGUARD_URL'],           # HTTPS proxy server (user:pass@host:port)
   # api_key: "Ar-ZG4zcP2T6IUb46ga7AIe-pdI5nM1byORM-J9nKnw771uv-MxRFD7gslcsi0gT",               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
