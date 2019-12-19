8deded7d4ef308441b02f7f8600eb30eedbd7acb
maxime fontana <maximefontana@hotmail.com>
2019-12-19 12:59:58 -0800

    updated to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,20 @@
 
 ActiveRecord::Schema.define(version: 2019_12_10_025319) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "date"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.float "latitude"
     t.float "longitude"
